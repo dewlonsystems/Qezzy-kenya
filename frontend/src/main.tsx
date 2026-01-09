f19a0a84fb441050e77b15e596f367bae61069ee
Deuk Mose <dewlonsystems@gmail.com>
2026-01-09 13:49:03 +0300

    SEO files3

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,11 @@
 // src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>,
 );
