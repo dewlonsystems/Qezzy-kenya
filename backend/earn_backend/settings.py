e64763321eab207ee425ee9d23262d696620cc9c
Deuk Mose <dewlonsystems@gmail.com>
2026-01-10 10:16:55 +0300

    pay

--- backend/earn_backend/settings.py
+++ backend/earn_backend/settings.py
@@ -154,4 +154,4 @@ def setup_search_path(sender, connection, **kwargs):
     with connection.cursor() as cursor:
         cursor.execute("SET search_path TO private, public;")
 
-PAYMENTS_ENABLED = True        
+PAYMENTS_ENABLED = False       
