dd6cbbfe450693644a0ef65b04a0ddfa7ca2bbee
Deuk Mose <dewlonsystems@gmail.com>
2026-01-21 10:46:49 +0300

    server1

--- backend/earn_backend/asgi.py
+++ backend/earn_backend/asgi.py
@@ -1,26 +1,19 @@
 # earn_backend/asgi.py
-"""
-ASGI config for earn_backend project.
-"""
-
 import os
 import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-from support.middleware import FirebaseTokenAuthMiddlewareStack
-import support.routing
+from channels.routing import ProtocolTypeRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'earn_backend.settings')
-
-# 👇 ADD THIS: Initialize Django ASGI application early
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "earn_backend.settings")
 django.setup()
 
+# 👇 Import ONLY AFTER django.setup()
 from django.core.asgi import get_asgi_application
+from support.middleware import FirebaseTokenAuthMiddlewareStack
+import support.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": FirebaseTokenAuthMiddlewareStack(
-        URLRouter(
-            support.routing.websocket_urlpatterns
-        )
+        support.routing.websocket_urlpatterns
     ),
 })
