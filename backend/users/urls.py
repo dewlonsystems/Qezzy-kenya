adcc196b1d73a4f4cb2cbc167ca0feb592612fed
Dewlon Systems <dewlonsystems@gmail.com>
2026-04-06 17:14:27 +0300

    ghkajjs

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('me/', views.UserDetailView.as_view(), name='user-detail'),
     path('profile/', views.ProfileUpdateView.as_view(), name='profile-update'),
     path('close/', views.CloseAccountView.as_view(), name='close-account'),
-    path('api/users/email-preferences/<str:token>/', views.EmailPreferencesView.as_view(), name='api_email_preferences'),
-    path('api/users/email-preferences/unsubscribe/<str:email_type>/', views.EmailUnsubscribeView.as_view(), name='api_email_unsubscribe'),
+    path('email-preferences/<str:token>/', views.EmailPreferencesView.as_view(), name='api_email_preferences'),
+    path('email-preferences/unsubscribe/<str:email_type>/', views.EmailUnsubscribeView.as_view(), name='api_email_unsubscribe'),
 ]
