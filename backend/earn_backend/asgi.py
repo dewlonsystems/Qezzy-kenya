8e9bd7ed7f69be7b758ffda5a984ea8133de7d6f
dewlonsystems <dewlonsystems@gmail.com>
2026-04-10 00:22:08 +0300

    Refactor ASGI application to use URLRouter

--- backend/earn_backend/asgi.py
+++ backend/earn_backend/asgi.py
@@ -1,18 +1,18 @@
 # earn_backend/asgi.py
 import os
 import django
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "earn_backend.settings")
 django.setup()
 
 from django.core.asgi import get_asgi_application
-from support.middleware import FirebaseTokenAuthMiddlewareStack
+from support.middleware import FirebaseTokenAuthMiddleware
 import support.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": FirebaseTokenAuthMiddlewareStack(
-        support.routing.websocket_urlpatterns
+    "HTTP": get_asgi_application(),
+    "websocket": FirebaseTokenAuthMiddleware(
+        URLRouter(support.routing.websocket_urlpatterns)
     ),
 })
