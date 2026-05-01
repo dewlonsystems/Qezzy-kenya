dec1f5a42fe6805554371da5da81f4f140b51568
Deuk Mose <dewlonsystems@gmail.com>
2026-05-02 02:07:06 +0300

    hhshssuisiu

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://api.qezzykenya.company',
         changeOrigin: true,
         secure: false,
       },
